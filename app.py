70385c2b47267307c14839bd74185eac8a111d0d
Ashnomay Roy <87134087+ashnoroy@users.noreply.github.com>
2026-08-01 11:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def run_agent(leader_agent, query):
     return code
 
     # leader_agent creation
-if all(all_API):    
+if all(ALL_API):    
   leader_agent = create_agent(
       model=model,
       tools=[
